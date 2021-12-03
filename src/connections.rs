dfe230e50186752505336f0f0ab1500480c92366
Tim van der Meulen <meulen@predic8.de>
2021-12-03 14:18:19 +0100

    current state

--- src/connections.rs
+++ src/connections.rs
@@ -1,10 +1,26 @@
-struct Element {
-    id: String,
-    connections: Vec<Connection>,
+#![feature(nll)]
+
+use std::collections::HashMap;
+use std::sync::{Mutex, Arc};
+
+pub(crate) struct Connection {
+    pub(crate) to_element: String,
+    pub(crate) score: i8
 }
 
-struct Connection {
-    to_element: Element,
-    score: u8,
-    locked: bool
+pub(crate) trait Connector<S> {
+    fn connect<T>(&mut self, con_handler: T) where T: Fn(&String, &mut S);
+}
+
+impl Connector<HashMap<String, Vec<Connection>>> for HashMap<String, Vec<Connection>> {
+    fn connect<T>(&mut self, con_handler: T) where T: Fn(&String, &mut HashMap<String, Vec<Connection>>) {
+        let keys= self.keys().cloned().collect::<Vec<_>>();
+        keys.iter().for_each(|id| con_handler(id, self));
+    }
+}
+
+impl Connector<HashMap<String, Vec<Connection>>> for Arc<Mutex<HashMap<String, Vec<Connection>>>> {
+    fn connect<T>(&mut self, con_handler: T) where T: Fn(&String, &mut HashMap<String, Vec<Connection>>) {
+        self.lock().unwrap().connect(con_handler);
+    }
 }
--- src/rules.rs
+++ src/rules.rs
@@ -1,32 +1,142 @@
-use serde::{Deserialize, Serialize};
+use connections::Connection;
 use derivative::Derivative;
+use regex;
+use score::Scorer;
+use serde::{Deserialize, Serialize};
+use std::collections::hash_map::RandomState;
 use std::collections::HashMap;
 
-#[derive(Serialize, Deserialize)]
-pub(crate) struct Rules {
-    references: HashMap<String, String>,
-    matching: Vec<Rule>,
-}
-
-#[derive(Derivative)]
-#[derive(Serialize, Deserialize)]
-struct Rule {
-    #[derivative(Default(value = "RuleType::IGNORE"))]
-    _type: RuleType,
-    #[derivative(Default(value = "None"))]
-    prio: Option<u64>,
-    #[derivative(Default(value = "RuleOperand::MATCH"))]
-    operand: RuleOperand,
+#[derive(Derivative, Serialize, Deserialize)]
+pub(crate) struct Rule {
+    #[derivative(Default(value = "RuleSeverity::IGNORE"))]
+    pub(crate) severity: RuleSeverity,
     #[derivative(Default(value = ""))]
-    field: String,
+    pub(crate) field: String,
+    #[derivative(Default(value = ""))]
+    pub(crate) target_field: String,
+    #[derivative(Default(value = "RuleOperand::MATCH"))]
+    pub(crate) operand: RuleOperand,
 }
 
 #[derive(Serialize, Deserialize)]
-enum RuleType {
-    Force, Prefer, ForceExclude, PreferExclude, Ignore
+pub(crate) enum RuleSeverity {
+    Force,
+    Prefer,
+    Standard,
+    PreferExclude,
+    ForceExclude,
 }
 
 #[derive(Serialize, Deserialize)]
-enum RuleOperand {
-    Match, Include
+pub(crate) enum RuleOperand {
+    Match,
+    Include,
 }
+
+trait RuleActions {
+    fn check(
+        &self,
+        id: &str,
+        target: &str,
+        values: &HashMap<String, HashMap<String, String>>,
+    ) -> bool;
+    fn apply(&self, id: &str, target: &str, connections: &mut HashMap<String, Vec<Connection>>);
+}
+
+impl RuleActions for Rule {
+    fn check(
+        &self,
+        id: &str,
+        target: &str,
+        values: &HashMap<String, HashMap<String, String>>,
+    ) -> bool {
+        let value = values
+            .get(id)
+            .expect(&*format!("No values found for target {}", id))
+            .get(&*self.field)
+            .expect(&*format!(
+                "Target {} has no value for {}",
+                id, self.target_field
+            ))
+            .trim();
+        let target_value = values
+            .get(target)
+            .expect(&*format!("No values found for target {}", target))
+            .get(&*self.target_field)
+            .expect(&*format!(
+                "Target {} has no value for {}",
+                target, self.target_field
+            ))
+            .trim();
+        return match self.operand {
+            RuleOperand::Match => value == target_value,
+            RuleOperand::Include => {
+                value.matches(format!("^(.*, *)?{}( *,.*|$)", regex::escape(target_value)))
+            }
+        };
+    }
+
+    fn apply(&self, id: &str, target: &str, connections: &mut HashMap<String, Vec<Connection>>) {
+        match self.severity {
+            RuleSeverity::Force
+            | RuleSeverity::Prefer
+            | RuleSeverity::Standard
+            | RuleSeverity::PreferExclude => {
+                let mut con = Connection {
+                    to_element: target.into_string(),
+                    score: self.severity.getScore(),
+                };
+                connections
+                    .get(id)
+                    .expect(&*format!("Missing element {}", id))
+                    .push(con);
+
+                // increase score for bidirectional references
+                // todo better logic and don't forget con from above
+                //if self.field != self.target_field {
+                //    let mut back_con = connections
+                //        .get(target)
+                //        .expect(&*format!("Missing element {}", target))
+                //        .iter_mut()
+                //        .find(|c| c.to_element == id);
+                //    if let Some(bc) = back_con {
+                //        if bc.score > 0 {
+                //            bc.score += RuleSeverity::Standard.getScore()
+                //        } else {
+                //            bc.score -= RuleSeverity::Standard.getScore()
+                //        }
+                //    }
+                //}
+            }
+            RuleSeverity::ForceExclude => {
+                // remove any connection between both
+                connections
+                    .get(id)
+                    .expect(&*format!("Missing element {}", id))
+                    .retain(|&c| c.to_element == target);
+                connections
+                    .get(target)
+                    .expect(&*format!("Missing element {}", target))
+                    .retain(|&c| c.to_element == id);
+            }
+        }
+    }
+}
+
+// trait RuleApplier<T> {
+//     fn apply(&self, id: String, apply_to: &mut T, target: String, apply_to: &mut T);
+// }
+//
+// impl RuleApplier<Vec<Connection>> for Rule {
+//     fn apply(&self, id: String, cons: &mut Vec<Connection>) {
+//         let mut locked = matches!(self.severity, RuleSeverity::Force);
+//         for con in cons {
+//             match self.operand {
+//                 RuleOperand::Match => {}
+//                 RuleOperand::Include => {}
+//             }
+//         }
+//
+//         cons.push(Connection { to_element: id, score: self.severity.getScore(), locked: matches!(self.severity, RuleSeverity::Force) });
+//     }
+// }
