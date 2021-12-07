56f9991d1386896a5be65690968f96402bfc29ef
Tim van der Meulen <meulen@predic8.de>
2021-12-07 12:42:28 +0100

    added next filter stages

--- src/connections.rs
+++ src/connections.rs
@@ -12,7 +12,9 @@ pub(crate) struct Connections {
 pub(crate) trait Connector<S, T> {
     fn from_data(data: &T) -> Self;
     fn apply(&mut self, to_apply: &S, extra_info: &T);
-    fn calc_score(&self, group: Vec<usize>) -> (bool, i32);
+    fn calc_score(&self, group: &Vec<&usize>) -> (bool, i32);
+    fn possible_connections(&self) -> Vec<Vec<usize>>;
+    //fn find_highest_score(&self, possible_connections: &Vec<Vec<i16>>) -> (i64, Vec<Vec<i16>>);
 }
 
 impl Connector<Rule, HashMap<String, HashMap<String, String>>> for Connections {
@@ -55,7 +57,7 @@ impl Connector<Rule, HashMap<String, HashMap<String, String>>> for Connections {
         }
     }
 
-    fn calc_score(&self, group: Vec<usize>) -> (bool, i32) {
+    fn calc_score(&self, group: &Vec<&usize>) -> (bool, i32) {
         let mut score: i32 = 0;
         let size = group.len();
         for x in 0..size {
@@ -63,33 +65,54 @@ impl Connector<Rule, HashMap<String, HashMap<String, String>>> for Connections {
                 if x == y {
                     continue;
                 }
-                if self.matrix[group[x]][group[y]] == i16::MIN {
+                if self.matrix[*group[x]][*group[y]] == i16::MIN {
                     return (false, i32::MIN);
                 }
-                score += self.matrix[group[x]][group[y]] as i32;
+                score += self.matrix[*group[x]][*group[y]] as i32;
             }
         }
         return (true, score);
+    }
+
+    fn possible_connections(&self) -> Vec<Vec<usize>> {
+        let size = self.fixed_order.len();
+        let mut possible_connections: Vec<Vec<usize>> = Vec::new();
+        for row in 0..size {
+            let mut connections: Vec<usize> = Vec::new();
+            connections.push(row);
+            for col in 0..size {
+                if row != col
+                    && self.matrix[row][col] != i16::MIN
+                    && self.matrix[col][row] != i16::MIN
+                {
+                    connections.push(col);
+                }
+            }
+            possible_connections.push(connections);
+        }
+        return possible_connections;
     }
 }
 
 impl fmt::Display for Connections {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let size = 6;
-        writeln!(f);
-        write!(f, "{0: <width$}", " ", width = size);
+        write!(f, "{0: <width$}", " ", width = size).expect("issue while printing connection");
         for element in &self.fixed_order {
-            write!(f, " | {0: <width$}", element, width = size);
+            write!(f, " | {0: <width$}", element, width = size)
+                .expect("issue while printing connection");
         }
-        writeln!(f);
-        writeln!(f, "{}", "-".repeat(10 * size));
+        writeln!(f).expect("issue while printing connection");
+        writeln!(f, "{}", "-".repeat(10 * size)).expect("issue while printing connection");
         let size = self.matrix.len();
         for row in 0..size {
-            write!(f, "{0: <width$}", self.fixed_order[row], width = size);
+            write!(f, "{0: <width$}", self.fixed_order[row], width = size)
+                .expect("issue while printing connection");
             for col in 0..size {
-                write!(f, " | {0: >width$}", self.matrix[row][col], width = size);
+                write!(f, " | {0: >width$}", self.matrix[row][col], width = size)
+                    .expect("issue while printing connection");
             }
-            writeln!(f);
+            writeln!(f).expect("issue while printing connection");
         }
         writeln!(f)
     }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+extern crate itertools;
 extern crate regex;
 extern crate serde;
 extern crate serde_json;
 
 use connections::{Connections, Connector};
-use rules::{Rule, RuleActions};
+use debug_print::debug_println;
+use itertools::Itertools;
+use rules::Rule;
 use serde::{Deserialize, Serialize};
+use std::cmp::max;
 use std::collections::HashMap;
 use std::fs;
 
@@ -17,7 +21,7 @@ struct MatchingData {
     fields: HashMap<String, String>, // id -> field name/text
     elements: HashMap<String, HashMap<String, String>>, // id -> { fieldId -> value }
     rules: Vec<Rule>,
-    outputs: HashMap<u16, u16>, // possible output sizes: size -> max usages
+    outputs: HashMap<usize, i16>, // possible output sizes: size -> max usages
 }
 
 fn main() {
@@ -26,13 +30,54 @@ fn main() {
         serde_json::from_str(&data).expect("JSON was not well-formatted");
 
     let mut connections = Connections::from_data(&matching_data.elements);
-    println!("{}", connections.to_string());
+    //println!("{}", connections.to_string());
     for rule in matching_data.rules {
-        println!("{}", rule.to_string());
+        //println!("{}", rule.to_string());
         connections.apply(&rule, &matching_data.elements);
-        println!("{}", connections.to_string());
+        //println!("{}", connections.to_string());
     }
 
-    println!("final result:");
-    println!("{}", connections.to_string());
+    debug_println!("preferences:");
+    debug_println!("{}", connections.to_string());
+
+    let mut possible_connections = connections.possible_connections();
+    possible_connections.sort_by(|con1, con2| con1.len().cmp(&con2.len()));
+    let connection_column_width = max(
+        20,
+        possible_connections[possible_connections.len() - 1].len(),
+    );
+    debug_println!(
+        "{0: <5} | {1: <connection_column_width$} | {2: <connection_column_width$} | {3: <10}",
+        "Index",
+        "possible connections",
+        "best combination",
+        "best score",
+        connection_column_width = connection_column_width
+    );
+    debug_println!("{}", "-".repeat(25 + 2 * connection_column_width));
+    possible_connections.iter().for_each(|item| {
+        //print!("{} -> {{ {}", item[0], item[0]);
+        let mut combinations: Vec<(i32, Vec<&usize>)> = Vec::new();
+        for output in &matching_data.outputs {
+            let combinations_of_size = item.iter().skip(1).combinations(*output.0 - 1);
+            for mut comb in combinations_of_size {
+                comb.insert(0, &item[0]);
+                let (possible, score) = connections.calc_score(&comb);
+                if possible {
+                    combinations.push((score, comb));
+                }
+            }
+        }
+        // sort by score
+        combinations.sort_by(|comb1, comb2| comb2.0.cmp(&comb1.0));
+
+        debug_println!(
+            "{0: <5} | {1: >connection_column_width$} | {2: >connection_column_width$} | {3: >10}",
+            item[0],
+            format!("{:?}", item),
+            format!("{:?}", combinations[0].1),
+            combinations[0].0,
+            connection_column_width = connection_column_width
+        );
+    });
 }
